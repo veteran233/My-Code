a7b69060fb85ebcd7572ebf8dfcd1cadd7ce9c66
veteran233 <a1328906205@live.com>
2020-02-18 14:51:47 +0800

    Update and rename LineSeparete.cpp to LineSegmentSeparete.cpp

--- LineSegmentSeparete.cpp
+++ LineSegmentSeparete.cpp
@@ -37,19 +37,13 @@ bool IsRectSeparate(const point &p1, const point &p2, const point &p3, const poi
 }
 bool IsLineSegmentSeparate(const point &p1, const point &p2, const point &p3, const point &p4)
 {
+	if (IsRectSeparate(p1, p2, p3, p4))
+		return 1;
+
 	if (
 		Array(Vector(p1, p2), Vector(p1, p3))*Array(Vector(p1, p2), Vector(p1, p4)) > 0 ||
 		Array(Vector(p3, p4), Vector(p3, p1))*Array(Vector(p3, p4), Vector(p3, p2)) > 0
 		)
-		return 1;
-	else
-		return 0;
-}
-bool IsLineSeparate(const point &p1, const point &p2, const point &p3, const point &p4)
-{
-	if (IsRectSeparate(p1, p2, p3, p4))
-		return 1;
-	if (IsLineSegmentSeparate(p1, p2, p3, p4))
 		return 1;
 	else
 		return 0;
