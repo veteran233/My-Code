17218ddc03e6fb5732e12122e13c7ebcc8bc8188
veteran233 <a1328906205@live.com>
2020-02-25 13:12:43 +0800

    Update USACO_fracdec.cpp

--- USACO/USACO_fracdec.cpp
+++ USACO/USACO_fracdec.cpp
@@ -4,26 +4,88 @@ PROG: fracdec
 LANG: C++11
 */
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-int N, D;
+vector<int> num;
+vector<bool> exist(100001, 0);
+vector<int> nex(100001);
+int a;
 
+int ncha(int x)
+{
+	if (x == 0)
+		return 1;
+	int ret = 0;
+	while (x != 0)
+	{
+		x /= 10;
+		ret++;
+	}
+	return ret;
+}
+bool Div(int N, int D)
+{
+	for (int i = 0;; i++)
+	{
+		if (exist[N%D])
+		{
+			a = nex[N%D];
+			break;
+		}
+		if (N%D != 0)
+		{
+			num.push_back(N%D);
+			exist[N%D] = 1;
+			nex[N%D] = i;
+		}
+		else
+			return 0;
+		N = N % D * 10;
+	}
+	return 1;
+}
 int main()
 {
-	/*freopen("fracdec.in", "r", stdin);
-	freopen("fracdec.out", "w", stdout);*/
+	ifstream fin("fracdec.in");
+	ofstream fout("fracdec.out");
 
-	cin >> N >> D;
+	int N, D;
+	fin >> N >> D;
 
-	cout << N / D << ".";
+	bool sign = Div(N, D);
+	int n = ncha(N / D) + 1;
 
-	for (int i = 0; i < 100; i++)
+	fout << N / D << ".";
+
+	for (int i = 0;; i++)
 	{
+		if (i == a && sign)
+		{
+			fout << "(";
+			n++;
+			if (n % 76 == 0)
+				fout << "\n";
+		}
+		if (sign && i >= num.size())
+			break;
+
 		N = N % D * 10;
-		cout << N / D;
+		fout << N / D;
+		n++;
+		if (n % 76 == 0)
+			fout << "\n";
+
+		if (N == 0 || i >= num.size() - 1)
+			break;
 	}
+
+	if (sign)
+		fout << ")" << endl;
+	else
+		fout << "\n";
 
 	return 0;
 }
