daa61a7c09ccb13889193cc1fdd75da4819c6c38
veteran233 <a1328906205@live.com>
2020-02-28 23:53:56 +0800

    Create USACO_humble.cpp

---
+++ USACO/USACO_humble.cpp
@@ -0,0 +1,21 @@
+/*
+ID: kookinhard
+PROG: humble
+LANG: C++11
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int K, N;
+
+int main()
+{
+	/*freopen("humble.in", "r", stdin);
+	freopen("humble.out", "w", stdout);*/
+
+
+
+	return 0;
+}
