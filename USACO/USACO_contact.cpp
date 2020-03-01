0db6da75a7a6c6b2d22828b625091b51ac3b70ff
veteran233 <a1328906205@live.com>
2020-03-02 02:51:52 +0800

    Update USACO_contact.cpp

--- USACO/USACO_contact.cpp
+++ USACO/USACO_contact.cpp
@@ -4,175 +4,135 @@ PROG: contact
 LANG: C++11
 */
 #define _CRT_SECURE_NO_WARNINGS
-#include <fstream>
+#include <iostream>
 #include <algorithm>
-#include <string>
 #include <vector>
-#include <deque>
+#include <string>
+#include <math.h>
 
 using namespace std;
 
+struct pattern
+{
+	pattern(int a, string str)
+	{
+		int_count = a;
+		str_pattern = str;
+	}
+	int int_count;
+	string str_pattern;
+};
+
 int A, B, N;
 string record;
-vector<string> pattern;
-vector<vector<string*>> point;
-vector<int> pat_count;
-vector<int> num;
+vector<pattern> pat;
 
-deque<int> prefix(const string &a)
+int pat_transform(string a)
 {
-	if (a.size() == 1)
-		return deque<int>(1, -1);
-	deque<int> ret((int)a.size());
-	ret[0] = 0;
-	int i = 0;
-	int j = 1;
-	while (j < a.size())
-	{
-		if (a[i] == a[j])
-			ret[j++] = ++i;
-		else
-		{
-			ret[j++] = 0;
-			i = 0;
-		}
-	}
-	ret.pop_back();
-	ret.push_front(-1);
-	return ret;
-}
-int kmp(const string &a)
-{
-	deque<int> p = prefix(a);
 	int ret = 0;
-	int i = 0;
-	while (i < record.size())
-	{
-		int j = 0;
-		while (j < a.size() && i < record.size())
-		{
-			if (a[j] == record[i])
-			{
-				++i;
-				++j;
-			}
-			else
-			{
-				j = p[j];
-				if (j == -1)
-				{
-					j = 0;
-					++i;
-				}
-			}
-		}
-		if (j == a.size())
-		{
-			++ret;
-			i = i - j + 1;
-		}
-	}
+	for (int i = a.size() - 1; i >= 0; --i)
+		if (a[i] == '1')
+			ret += (int)pow(2, (int)a.size() - i - 1);
 	return ret;
 }
-void dfs(int level = 0, string str = "")
+bool cmp1(pattern *a, pattern *b)
 {
-	if (level > B)
-		return;
+	if (a->str_pattern.size() < b->str_pattern.size())
+		return 1;
 	else
-	{
-		if (str.size() >= A)
-			pattern.push_back(str);
-		str.push_back('0');
-		dfs(level + 1, str);
-
-		*(--str.end()) = '1';
-		dfs(level + 1, str);
-	}
+		if (a->str_pattern.size() == b->str_pattern.size())
+			if (a->str_pattern < b->str_pattern)
+				return 1;
+			else
+				return 0;
+		else
+			return 0;
 }
-//bool cmp(int &a, int &b)
-//{
-//	if (pat_count[a] > pat_count[b])
-//		return 1;
-//	else
-//		return 0;
-//}
-//void ansprint(int index)
-//{
-//	fout << pat_count[index] << endl;
-//	for (int i=0;i<)
-//}
+bool cmp2(pattern &a, pattern &b)
+{
+	if (a.int_count > b.int_count)
+		return 1;
+	else
+		return 0;
+}
 
 int main()
 {
-	ifstream fin("contact.in");
-	ofstream fout("contact.out");
+	freopen("contact.in", "r", stdin);
+	freopen("contact.out", "w", stdout);
 
-	fin >> A >> B >> N;
+	cin >> A >> B >> N;
 
-	string str_temp;
-	while (fin >> str_temp)
-		record.append(str_temp);
+	string user_temp;
+	while (cin >> user_temp)
+		record.append(user_temp);
 
-	dfs();
+	vector<int> point(B - A + 1, 0);
+	vector<int> len(B - A + 1);
+	for (int i = A; i < B + 1; ++i)
+		len[i - A] = i;
 
-	vector<bool> visited((int)pattern.size(), 0);
-
-	for (int i = 0; i < pattern.size(); ++i)
-		pat_count.push_back(kmp(pattern[i]));
-
-	//sort(num.begin(), num.end(), cmp);
-
-	int vis = 0;
-	int index = 0;
-	while (vis < pattern.size())
+	vector<vector<bool>> visited;
+	vector<vector<int>> pos;
+	for (int i = 0; i < B - A + 1; ++i)
 	{
-		for (; visited[index]; ++index);
-		int pmax = index;
-		for (int i = index; i < pattern.size(); ++i)
-			if (!visited[i] && pat_count[pmax] < pat_count[i])
-				pmax = i;
+		visited.push_back(vector<bool>((int)pow(2, len[i]), 0));
+		pos.push_back(vector<int>((int)pow(2, len[i])));
 
-		point.push_back(vector<string*>(1, &pattern[pmax]));
-		num.push_back(pat_count[pmax]);
-		visited[pmax] = 1;
-		++vis;
-		for (int i = pmax + 1; i < pattern.size(); ++i)
-			if (!visited[i] && pat_count[pmax] == pat_count[i])
+		for (int j = len[i] - 1; j < record.size(); ++j)
+		{
+			int index = pat_transform(record.substr(j - len[i] + 1, len[i]));
+			if (!visited[i][index])
 			{
-				point[(int)point.size() - 1].push_back(&pattern[i]);
-				visited[i] = 1;
-				++vis;
+				pat.push_back(pattern(1, record.substr(j - len[i] + 1, len[i])));
+				pos[i][index] = (int)pat.size() - 1;
+				visited[i][index] = 1;
 			}
+			else
+				++pat[pos[i][index]].int_count;
+		}
 	}
 
-	if (point.size() < N)
-		for (int i = 0; i < point.size() && num[i] > 0; ++i)
+	sort(pat.begin(), pat.end(), cmp2);
+
+	vector<vector<pattern*>> ans;
+	int index = 0;
+	int now_count = -1;
+
+	while (ans.size() < N + 1 && index < pat.size())
+	{
+		if (now_count != pat[index].int_count)
 		{
-			fout << num[i] << endl;
-			for (int j = 1; j <= point[i].size(); ++j)
+			ans.push_back(vector<pattern*>());
+			if (ans.size() == N + 1)
 			{
-				fout << *point[i][j - 1];
-				if (j % 6 != 0 && j != point[i].size())
-					fout << " ";
-				else if (j != point[i].size())
-					fout << "\n";
+				ans.pop_back();
+				break;
 			}
-			fout << "\n";
+			ans[(int)ans.size() - 1].push_back(&pat[index]);
+			now_count = pat[index].int_count;
 		}
-	else
-		for (int i = 0; i < N && num[i] > 0; ++i)
+		else
+			ans[(int)ans.size() - 1].push_back(&pat[index]);
+		index++;
+	}
+
+	for (int i = 0; i < ans.size(); ++i)
+		sort(ans[i].begin(), ans[i].end(), cmp1);
+
+	for (int i = 0; i < ans.size(); ++i)
+	{
+		cout << ans[i][0]->int_count << endl;
+		for (int j = 1; j <= ans[i].size(); ++j)
 		{
-			fout << num[i] << endl;
-			for (int j = 1; j <= point[i].size(); ++j)
-			{
-				fout << *point[i][j - 1];
-				if (j % 6 != 0 && j != point[i].size())
-					fout << " ";
-				else if (j != point[i].size())
-					fout << "\n";
-			}
-			fout << "\n";
+			cout << ans[i][j - 1]->str_pattern;
+			if (j % 6 != 0 && j != ans[i].size())
+				cout << " ";
+			else if (j % 6 == 0 && j != ans[i].size())
+				cout << "\n";
 		}
+		cout << "\n";
+	}
 
 	return 0;
 }
