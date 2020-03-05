ab2d935fa5d3606d6f6c8bab9d979930e64d0fd5
veteran233 <a1328906205@live.com>
2020-03-05 22:08:11 +0800

    Create USACO_kimbits.cpp

---
+++ USACO/USACO_kimbits.cpp
@@ -0,0 +1,25 @@
+/*
+ID: kookinhard
+PROG: kimbits
+LANG: C++11
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int N, L, I;
+
+int main()
+{
+	/*freopen("kimbits.in", "r", stdin);
+	freopen("kimbits.out", "w", stdout);*/
+
+	cin >> N >> L >> I;
+
+
+
+	return 0;
+}
