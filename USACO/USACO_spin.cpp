1240540843a2b75cb81b6d127c38db885147d510
veteran233 <a1328906205@live.com>
2020-03-07 23:06:27 +0800

    Create USACO_spin.cpp

---
+++ USACO/USACO_spin.cpp
@@ -0,0 +1,17 @@
+/*
+ID: kookinhard
+PROG: spin
+LANG: C++11
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	/*freopen("spin.in", "r", stdin);
+	freopen("spin.out", "w", stdout);*/
+
+	return 0;
+}
