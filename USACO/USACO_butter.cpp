e0c710c8f2cfb7dce8ec451a54c201df37333839
veteran233 <a1328906205@live.com>
2020-03-18 23:52:27 +0800

    Update USACO_butter.cpp

--- USACO/USACO_butter.cpp
+++ USACO/USACO_butter.cpp
@@ -15,15 +15,15 @@ vector<int> cow_pos;
 const int inf = 0x3f3f3f3f;
 int ans = inf;
 
-int pathsum(int index, vector<vector<int>>&path)
+int distsum(vector<int>&dist)
 {
-	int psum = 0;
+	int ret = 0;
 	for (int i = 0; i < N; ++i)
-		if (path[index][cow_pos[i]] != inf)
-			psum += path[index][cow_pos[i]];
+		if (dist[cow_pos[i]] != inf)
+			ret += dist[cow_pos[i]];
 		else
 			return inf;
-	return psum;
+	return ret;
 }
 
 int main()
@@ -39,26 +39,45 @@ int main()
 		cow_pos.push_back(user_in);
 	}
 
-	vector<vector<int>> path(P + 1, vector<int>(P + 1, inf));
+	vector<vector<int>> weight(P + 1, vector<int>(P + 1, inf));
 
 	for (int i = 0; i <= P; ++i)
-		path[i][i] = 0;
+		weight[i][i] = 0;
 	for (int i = 0; i < C; ++i)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
-		path[a][b] = c;
-		path[b][a] = c;
+		weight[a][b] = c;
+		weight[b][a] = c;
 	}
 
-	for (int k = 0; k <= P; ++k)
-		for (int i = 0; i <= P; ++i)
-			for (int j = 0; j <= P; ++j)
-				if (path[i][k] + path[k][j] < path[i][j])
-					path[i][j] = path[i][k] + path[k][j];
+	int source = 0;
+	int mcount = 0;
+	while (mcount < P + 1)
+	{
+		vector<int> dist(P + 1, inf);
+		vector<bool> visited(P + 1, 0);
 
-	for (int i = 0; i <= P; ++i)
-		ans = min(ans, pathsum(i, path));
+		dist[source] = 0;
+
+		int index = 0;
+		int count = 0;
+		while (count <= P)
+		{
+			for (; visited[index]; ++index);
+			int i = index;
+			for (int j = index + 1; j <= P; ++j)
+				if (!visited[j] && dist[i] > dist[j])
+					i = j;
+			visited[i] = 1;
+			for (int j = 0; j <= P; ++j)
+				if (dist[j] > dist[i] + weight[i][j])
+					dist[j] = dist[i] + weight[i][j];
+			++count;
+		}
+		ans = min(ans, distsum(dist));
+		source = ++mcount;
+	}
 
 	cout << ans << endl;
 
