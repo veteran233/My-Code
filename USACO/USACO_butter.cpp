0bf54571ad1de6de0ba3398bdd2a5ab588e4846e
veteran233 <a1328906205@live.com>
2020-03-20 02:22:23 +0800

    Update USACO_butter.cpp

--- USACO/USACO_butter.cpp
+++ USACO/USACO_butter.cpp
@@ -7,6 +7,7 @@ LANG: C++11
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -15,41 +16,10 @@ vector<int> cow_pos;
 const int inf = 0x3f3f3f3f;
 int ans = inf;
 
-class cla_queue
-{
-public:
-	cla_queue(int i)
-	{
-		d = i;
-	}
-	int index;
-	int d;
-
-	bool operator <(const cla_queue&a)const
-	{
-		return this->d < a.d;
-	}
-	bool operator >(const cla_queue&a)const
-	{
-		return this->d > a.d;
-	}
-};
-
-int distsum(vector<int>&dist)
-{
-	int ret = 0;
-	for (int i = 0; i < N; ++i)
-		if (dist[cow_pos[i]] != inf)
-			ret += dist[cow_pos[i]];
-		else
-			return inf;
-	return ret;
-}
-
 int main()
 {
-	/*freopen("butter.in", "r", stdin);
-	freopen("butter.out", "w", stdout);*/
+	freopen("butter.in", "r", stdin);
+	freopen("butter.out", "w", stdout);
 
 	cin >> N >> P >> C;
 	for (int i = 0; i < N; ++i)
@@ -60,6 +30,7 @@ int main()
 	}
 
 	vector<vector<int>> weight(P + 1, vector<int>(P + 1, inf));
+	vector<vector<int>> neighbor(P + 1);
 
 	for (int i = 0; i <= P; ++i)
 		weight[i][i] = 0;
@@ -69,44 +40,50 @@ int main()
 		cin >> a >> b >> c;
 		weight[a][b] = c;
 		weight[b][a] = c;
+		neighbor[a].push_back(b);
+		neighbor[b].push_back(a);
 	}
 
-	int source = 0;
-	int nv = 0;
-	while (nv < P + 1)
+	vector<vector<int>> dist(N, vector<int>(P + 1, inf));
+
+	for (int nv = 0; nv < N; ++nv)
 	{
-		//vector<bool> visited(P + 1, 0);
-		vector<int> truedist(P + 1, inf);
+		vector<bool> visited(P + 1, 0);
+		vector<bool> joined(P + 1, 0);
+		list<int> join;
 
-		vector<cla_queue> dist(P + 1, cla_queue(inf));
-		for (int i = 0; i <= P; ++i)
-			dist[i].index = i;
+		dist[nv][cow_pos[nv]] = 0;
+		join.push_back(cow_pos[nv]);
 
-		dist[source].d = 0;
-
-		make_heap(dist.begin(), dist.end(), less<cla_queue>());
-
-		int numvisited = 0;
-		while (numvisited < P + 1)
+		for (int numvisited = 0; numvisited < P; ++numvisited)
 		{
-			int i = dist[0].index;
+			int i = *join.begin();
 
-			for (int j = 0; j <= P; ++j)
-				if (truedist[j] > truedist[i] + weight[i][j])
+			list<int>::iterator deletejoin = join.begin();
+			for (list<int>::iterator j = join.begin(); j != join.end(); ++j)
+				if (dist[nv][i] > dist[nv][*j])
 				{
-					truedist[j] = truedist[i] + weight[i][j];
-					dist[j].d = dist[i].d + weight[i][j];
+					i = *j;
+					deletejoin = j;
 				}
 
-			pop_heap(dist.begin(), dist.end(), less<cla_queue>());
-			++numvisited;
-		}
-		ans = min(ans, distsum(truedist));
-		++source;
-		++nv;
-	}
+			visited[i] = 1;
+			joined[i] = 1;
+			join.erase(deletejoin);
 
-	cout << ans << endl;
+			for (int t = 0; t < neighbor[i].size(); ++t)
+				if (!joined[neighbor[i][t]])
+				{
+					join.push_back(neighbor[i][t]);
+					joined[neighbor[i][t]] = 1;
+				}
+
+			if (!neighbor[i].empty())
+				for (int j = 0; j < (int)neighbor[i].size(); ++j)
+					if (dist[nv][neighbor[i][j]] > dist[nv][i] + weight[i][neighbor[i][j]])
+						dist[nv][neighbor[i][j]] = dist[nv][i] + weight[i][neighbor[i][j]];
+		}
+	}
 
 	return 0;
 }
