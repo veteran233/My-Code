b7e178505a4b6293634bc2615e03b75ff87909bb
veteran233 <a1328906205@live.com>
2020-03-23 00:54:19 +0800

    Update USACO_fence.cpp

--- USACO/USACO_fence.cpp
+++ USACO/USACO_fence.cpp
@@ -5,36 +5,30 @@ LANG: C++11
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 int F;
 int pos = 0;
-vector<vector<bool>> connect(501, vector<bool>(501, 0));
+vector<vector<int>> connect(501, vector<int>(501, 0));
 vector<vector<int>> neighbor(501);
-vector<int> tour(501, 0);
+vector<int> tour(1025, 0);
 
-void find_euler_tour(const int &index)
+void find_euler_tour(const int index)
 {
-	if ((int)neighbor[index].size() == 0)
-		tour[pos++] = index;
-	else
+	for (int i = 0; i < (int)neighbor[index].size(); ++i)
 	{
-		for (int i = 0; i < (int)neighbor[index].size(); ++i)
+		if (connect[index][neighbor[index][i]])
 		{
-			if (connect[index][neighbor[index][i]])
-			{
-				connect[index][neighbor[index][i]] = 0;
-				connect[neighbor[index][i]][index] = 0;
+			--connect[index][neighbor[index][i]];
+			--connect[neighbor[index][i]][index];
 
-				find_euler_tour(neighbor[index][i]);
-				i = -1;
-			}
-
+			find_euler_tour(neighbor[index][i]);
 		}
-		tour[pos++] = index;
 	}
+	tour[pos++] = index;
 }
 
 int main()
@@ -48,24 +42,30 @@ int main()
 	{
 		int a, b;
 		cin >> a >> b;
-		connect[a][b] = 1;
-		connect[b][a] = 1;
+		++connect[a][b];
+		++connect[b][a];
 
 		neighbor[a].push_back(b);
 		neighbor[b].push_back(a);
 	}
 
+	for (int i = 0; i < 501; ++i)
+		if ((bool)neighbor[i].size())
+			sort(neighbor[i].begin(), neighbor[i].end());
+
 	int index = 0;
-	for (; (int)neighbor[index].size() % 2 == 0; ++index);
-	if (index == 501)
+	while (index < 501)
 	{
-		index = 0;
-		for (; (int)neighbor[index].size() == 0; ++index);
+		if ((int)neighbor[index].size() % 2 != 0)
+			break;
+		++index;
 	}
+	if (index == 501)
+		for (index = 0; (int)neighbor[index].size() == 0; ++index);
 
 	find_euler_tour(index);
 
-	for (int i = 500; i >= 0; --i)
+	for (int i = 1024; i >= 0; --i)
 		if (tour[i])
 			cout << tour[i] << endl;
 
