476f517e9d6943fb6fda18b38d5c08994e690e53
veteran233 <a1328906205@live.com>
2020-03-24 22:21:06 +0800

    Create USACO_shopping.cpp

---
+++ USACO/USACO_shopping.cpp
@@ -0,0 +1,17 @@
+/*
+ID: kookinhard
+PROG: shopping
+LANG: C++11
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("shopping.in", "r", stdin);
+    freopen("shopping.out", "w", stdout);
+
+    return 0;
+}
