d094930efb2edf5f4b5bc1393349a7d485ca3f30
veteran233 <a1328906205@live.com>
2020-03-27 22:08:37 +0800

    Update USACO_shopping.cpp

--- USACO/USACO_shopping.cpp
+++ USACO/USACO_shopping.cpp
@@ -5,13 +5,63 @@ LANG: C++11
 */
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class cla_offer
+{
+public:
+	vector<int> code;
+	vector<int> num;
+	void setoffer(int c, int n)
+	{
+		code.push_back(c);
+		num.push_back(n);
+	}
+	int price;
+};
+class cla_item
+{
+public:
+	int code;
+	int num;
+	int price;
+};
+
+vector<cla_offer> offer;
+vector<cla_item> item;
+
 int main()
 {
-    freopen("shopping.in", "r", stdin);
-    freopen("shopping.out", "w", stdout);
+	/*freopen("shopping.in", "r", stdin);
+	freopen("shopping.out", "w", stdout);*/
 
-    return 0;
+	int user_in;
+
+	cin >> user_in;
+
+	offer.resize(user_in);
+
+	for (int i = 0; i < user_in; ++i)
+	{
+		int p;
+		cin >> p;
+		for (int j = 0; j < p; ++j)
+		{
+			int code, num;
+			cin >> code >> num;
+			offer[i].setoffer(code, num);
+		}
+		cin >> offer[i].price;
+	}
+
+	cin >> user_in;
+
+	item.resize(user_in);
+
+	for (int i = 0; i < user_in; ++i)
+		cin >> item[i].code >> item[i].num >> item[i].price;
+
+	return 0;
 }
