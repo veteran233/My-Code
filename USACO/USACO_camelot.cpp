419f022898db475d406f0903ab77908a3909a21a
veteran233 <a1328906205@live.com>
2020-03-30 23:21:46 +0800

    Create USACO_camelot.cpp

---
+++ USACO/USACO_camelot.cpp
@@ -0,0 +1,17 @@
+/*
+ID: kookinhard
+PROG: camelot
+LANG: C++11
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("camelot.in", "r", stdin);
+    freopen("camelot.out", "w", stdout);
+
+    return 0;
+}
