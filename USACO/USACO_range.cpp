198c1703026bb0df16d1ca41f606a4c458328564
veteran233 <a1328906205@live.com>
2020-04-06 13:26:22 +0800

    Create USACO_range.cpp

---
+++ USACO/USACO_range.cpp
@@ -0,0 +1,17 @@
+/*
+ID: kookinhard
+PROG: range
+LANG: C++11
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	freopen("range.in", "r", stdin);
+	freopen("range.out", "w", stdout);
+
+	return 0;
+}
