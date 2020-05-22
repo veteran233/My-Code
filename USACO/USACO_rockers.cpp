c65109ce85dfd00b2cc1c619cf382941093df2e0
veteran233 <a1328906205@live.com>
2020-05-23 02:48:04 +0800

    Create USACO_rockers.cpp

---
+++ USACO/USACO_rockers.cpp
@@ -0,0 +1,17 @@
+/*
+ID: kookinhard
+PROG: rockers
+LANG: C++11
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("rockers.in", "r", stdin);
+    freopen("rockers.out", "w", stdout);
+
+    return 0;
+}
